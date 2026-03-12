d3e089cf9b85c35060b0fef0a13c705159cef77c
Yashdeep <abyash14@gmail.com>
2026-03-12 17:32:29 +0530

    Change(9)

--- components/home-feed.tsx
+++ components/home-feed.tsx
@@ -193,14 +193,14 @@ export function HomeFeed() {
                                     <motion.div
                                         initial={{ opacity: 0 }}
                                         animate={{ opacity: 1 }}
-                                        className="columns-1 md:columns-2 lg:columns-3 xl:columns-4 gap-4 pb-20"
+                                        className="flex flex-col gap-6 max-w-2xl mx-auto pb-20"
                                     >
                                         {posts.map((post) => (
                                             <PostCard key={post.id} post={post} handleAuthAction={handleAuthAction} />
                                         ))}
 
                                         {/* Sentinel for Infinite Scroll */}
-                                        <div ref={lastPostRef} className="h-10 flex items-center justify-center break-inside-avoid w-full col-span-full">
+                                        <div ref={lastPostRef} className="h-10 flex items-center justify-center w-full">
                                             {loadingMore && <Loader2 className="w-6 h-6 animate-spin text-blue-500" />}
                                         </div>
                                     </motion.div>
